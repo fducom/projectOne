1cef85a8a9225095d2784ee0c0d9169236e212f4
wongsitu <sa_waika_4d_23@hotmail.com>
2018-08-29 14:45:48 -0700

    modified orders.js

--- models/orders.js
+++ models/orders.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model('Orders', 
-    new mongoose.Schema({
-        _user: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-            },
-        dishes: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Dish'
-            }],
-        totalPrice: Number
-    })
-);
-=======
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
@@ -35,4 +18,3 @@ const Order = mongoose.model('Order', orderSchema);
 
 module.exports = Order;
     
->>>>>>> ff8271a1f247be3daebc6b80382637860d2badaf
