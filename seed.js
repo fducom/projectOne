630d0cf3bd33bbf62f1da4b4920de3e932f93dcc
wongsitu <sa_waika_4d_23@hotmail.com>
2018-08-29 20:22:03 -0700

    Show one, show all, delete one dish

--- seed.js
+++ seed.js
@@ -1,7 +1,11 @@
+const db = require("./models");
+
 let dishes_list = [
     { name: 'eggs', completed: false , price: 12.00, description:"sadd" },
     { name: 'chicken', completed: false , price: 10.00, description:"asdds" },
     { name: 'yougurt', completed: false , price: 11.00, description:"asddsa" },
+    { name: 'milk', completed: false , price: 9.00, description:"asddsa" },
+    { name: 'chocolate', completed: false , price: 8.00, description:"asddsa" },
 ];
 
 db.Dish.remove({}, function(err, books){
--- server.js
+++ server.js
@@ -22,7 +22,14 @@ app.get('/', (req, res) => {
 //  DISHES
 ///////////////////
 
-//All dishes, show each dish
+//Show one dish
+app.get('/api/dishes/:id', function (req, res) {
+    db.Dish.findOne({_id: req.params.id }, function(err, data) {
+        res.json(data);
+    });
+});
+
+//Show all dish
 app.get('/api/dishes', function (req, res) {
     // send all books as JSON response
     db.Dish.find(function(err, dishes){
@@ -34,16 +41,40 @@ app.get('/api/dishes', function (req, res) {
     });
 });
 
+// Destroy one dish
+app.delete('/api/dishes/:id', (req, res) => {
+    // get todo id from url params (`req.params`)
+    let dishId = req.params.id;
+    // find todo in db by id and delete
+    db.Dish.deleteOne(
+        { _id: dishId },
+        (err, deletedDish) => {
+            if(err){ return res.status(400).json({err: "error has occured"})}
+            res.json({data:deletedDish});
+    });
+});
+
 ////////////////////
 //  ORDERS
 ///////////////////
+
+app.get("api/orders", (req,res) =>{
+    db.Dish.find(function(err, orders){
+        if (err) {
+            console.log("index error: " + err);
+            res.sendStatus(500);
+        }
+        res.json({data:orders});
+    });
+})
+
 
 // Create order
 app.post('/api/order', (req, res) => {
     /* This endpoint will add a todo to our "database"
      * and respond with the newly created todo.
      */
-    const newTodo = req.body;
+    const newOrder = req.body;
     newTodo._id  = parseInt(todos.length);
     todos.push(newTodo);
     res.status(200).json(newTodo);
