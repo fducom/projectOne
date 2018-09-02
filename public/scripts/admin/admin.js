775a90383ada389ffba5633d1c47ed334380b07c
wongsitu <sa_waika_4d_23@hotmail.com>
2018-09-02 13:33:16 -0700

    Admin DELETE working

--- public/scripts/admin/admin.js
+++ public/scripts/admin/admin.js
@@ -6,37 +6,32 @@ $(document).ready(function(){
         url: 'http://localhost:3000/api/orders',  
         success: function (json){
             for(let i = 0; i < json.data.length ; i++){
-                $("#ControlList").append(   `<div id="number${i}" style="border-bottom: 2px solid white">
+                $("#ControlList").append(   `<div class="Ord" id="number${i}" style="border-bottom: 2px solid white">
                                                 Order number: ${i}
-                                                <div id="order-${i}">
-                                                <input value="${json.data[i]._id}" type='hidden'>
-                                                </div>
+                                                <div id="order-${i}"></div>
                                             </div>`);
                 for(let j = 0; j < json.data[i].dishes.length ;j++){
                     $(`#order-${i}`).append(`<li>${json.data[i].dishes[j].name}</li>`);
                 }
-                $(`#number${i}`).append(`<p>
-                                            Total price: $${json.data[i].totalPrice} 
-                                            <button class="DeleteBtn">Delete Order</button>
-                                        </p>`);
+                $(`#number${i}`).append(`Total price: $${json.data[i].totalPrice}
+                                        <input value="${json.data[i]._id}" type='hidden'> 
+                                        <button class="del"> Delete</button>`);
             }
+            $("#ControlList").children("div").on("click", ".del", function(e){
+                e.preventDefault();
+                let delOrderId = $(this).siblings("input").attr("value");
+                //AJAX request to delete an existing order
+                $.ajax({
+                    method: "DELETE",
+                    url: `http://localhost:3000/api/orders/${delOrderId}`,
+                    success: json =>{
+                        console.log("it's working")
+                    },
+                });
+            });
         },
         error: function(error){
             console.log(error);
-        },
-    });
-
-    $("#ControlList").on('click',".DeleteBtn", event =>{
-        console.log($(this).parent().siblings("div").find("input"));
-        let delOrderId = $();
-        //AJAX request to delete one order
-        // $.ajax({
-        //     method: "DELETE",
-        //     url: 'http://localhost:3000/api/orders',
-        //     data: delOrderId,
-        //     success: json =>{
-        //         
-        //     },
-        // });
+        }
     });
 })
